8cc3c97fa424e078ee44334b62aa25f53dd4b5a2
Iszak Bryan <iszak.bryan@gmail.com>
2015-05-30 22:31:49 +0200

    Separated directory structures into skeletons

---
+++ manifests/skeleton/default.pp
@@ -0,0 +1,15 @@
+define project::skeleton::default (
+    $owner,
+    $group
+    $user
+) {
+    $public_path  = "${home_path}/public"
+
+    file { $public_path:
+        ensure  => directory,
+        require => User[$user],
+        path    => $public_path,
+        owner   => $owner,
+        group   => $group
+    }
+}
