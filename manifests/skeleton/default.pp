ca7f2375f1222622e5a0a2b6bfff8526f5f28789
Iszak Bryan <iszak.bryan@gmail.com@git config --global user.name Iszak>
2015-07-30 18:29:42 -0400

    Removed unused variables

--- manifests/skeleton/default.pp
+++ manifests/skeleton/default.pp
@@ -3,12 +3,10 @@ define project::skeleton::default (
     $group,
     $user,
     $project,
+    $project_path,
 ) {
     validate_string($user)
     validate_string($owner)
     validate_string($group)
     validate_string($project)
-
-    $home_path    = "/home/${user}"
-    $public_path  = "${home_path}/public"
 }
